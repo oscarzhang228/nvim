84455e820f3d415750a3fedbfe43c6597e3c12f9
Oscar Zhang <oscarzhang228@gmail.com>
2025-02-20 17:46:45 -0500

    add treesitter parsers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,19 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Treesitter
 
 ---@type LazySpec
+
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     ensure_installed = {
       "lua",
       "vim",
-      -- add more arguments for adding more treesitter parsers
+      "python",
+      "typescript",
+      "javascript",
+      "cpp",
+      "java",
+      "lua",
     },
   },
 }
