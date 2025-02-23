2115e832630748c826142e4db1cfd5a0a2e7039e
Oscar Zhang <oscarzhang228@gmail.com>
2025-02-23 18:45:34 -0500

    add plugin for surround in nvim

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -226,6 +226,18 @@ return {
     end,
   },
 
+  -- Surround.nvim to deal with surrounding stuff
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
+
   -- Git Conflict Management
   { "akinsho/git-conflict.nvim", version = "*", config = true },
   {
