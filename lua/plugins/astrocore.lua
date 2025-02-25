9c298bb0863678606baf76805d5fba0b10166d90
Oscar Zhang <oscarzhang228@gmail.com>
2025-02-25 14:21:48 -0500

    add keycode for <Leader>fi which finds incoming references

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -37,18 +37,10 @@ return {
         -- This can be found in the `lua/lazy_setup.lua` file
       },
     },
-    -- Mappings can be configured through AstroCore as well.
-    -- NOTE: keycodes follow the casing in the vimdocs. For example, `<Leader>` must be capitalized
     mappings = {
-      -- first key is the mode
       n = {
-        -- second key is the lefthand side of the map
-
-        -- navigate buffer tabs
         ["]b"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
         ["[b"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
-
-        -- mappings seen under group name "Buffer"
         ["<Leader>bd"] = {
           function()
             require("astroui.status.heirline").buffer_picker(
@@ -57,15 +49,11 @@ return {
           end,
           desc = "Close buffer from tabline",
         },
-
         ["<Leader>tm"] = { function() require("toggleterm-manager").open {} end, desc = "Open toggleterm-manager" },
-
-        -- tables with just a `desc` key will be registered with which-key if it's installed
-        -- this is useful for naming menus
-        -- ["<Leader>b"] = { desc = "Buffers" },
-
-        -- setting a mapping to false will disable it
-        -- ["<C-S>"] = false,
+        ["<Leader>fi"] = {
+          function() require("telescope.builtin").lsp_references() end,
+          desc = "Find incoming references",
+        },
       },
       t = {
         ["<Esc>"] = [[<C-\><C-n>]],
