014da05ef35fcd0d0bea3ad4e99b149a6c9cc0ee
Oscar Zhang <oscarzhang228@gmail.com>
2025-03-07 00:18:56 -0500

    change to catppuccin machiato for my colorscheme

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -4,17 +4,8 @@ return {
     "AstroNvim/astroui",
     ---@type AstroUIOpts
     opts = {
-      -- change colorscheme
-      colorscheme = "astrodark",
-      -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
-      highlights = {
-        init = { -- this table overrides highlights in all themes
-          -- Normal = { bg = "#000000" },
-        },
-        astrodark = { -- a table of overrides/changes when applying the astrotheme theme
-          -- Normal = { bg = "#000000" },
-        },
-      },
+      transparent = true,
+      colorscheme = "catppuccin-macchiato",
       -- Icons can be configured throughout the interface
       icons = {
         -- configure the loading of the lsp in the status line
