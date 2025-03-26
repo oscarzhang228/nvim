97661d391b615cb691cdd85ef35e60a558203303
Oscar Zhang <oscarzhang228@gmail.com>
2025-03-26 17:54:37 -0400

    add snippets for console log

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -27,4 +27,15 @@ return {
     "ThePrimeagen/harpoon",
     dependencies = { "nvim-lua/plenary.nvim" },
   },
+  {
+    "L3MON4D3/LuaSnip",
+    config = function(plugin, opts)
+      -- include the default astronvim config that calls the setup call
+      require "astronvim.plugins.configs.luasnip"(plugin, opts)
+      -- load snippets paths
+      require("luasnip.loaders.from_vscode").lazy_load {
+        paths = { vim.fn.stdpath "config" .. "/snippets" },
+      }
+    end,
+  },
 }
